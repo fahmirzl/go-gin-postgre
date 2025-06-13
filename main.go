db63eec15156a99dda3019ae37fa880711dd6ec1
fahmirzl <fahmirizal.dev@gmail.com>
2025-06-13 19:39:45 +0700

    #

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"go-gin-postgre/routers"
 	"os"
 )
 
 func main() {
 	// database.DBMigrate(database.DB)
-	routers.StartServer().Run(":" + os.Getenv("PORT"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Running on port:", port)
+	routers.StartServer().Run(":" + port)
 }
